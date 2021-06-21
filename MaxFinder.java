e16f0a0c1e4b7016afcdd2087c89a4f1a0b6b64e
hsa-98 <harshsanjayagrawal89@gmail.com>
2021-06-21 22:57:56 +0530

    The code is refactored to take in array and a print function is added

--- MaxFinder.java
+++ MaxFinder.java
@@ -1,43 +1,48 @@
 package com.harsh.max;
 
+import com.sun.source.tree.SwitchExpressionTree;
+
+import java.lang.invoke.SwitchPoint;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MaxFinder<E extends Comparable <E>> {
-    E var1,var2,var3;
-    public MaxFinder(E var1,E  var2, E var3){
-        this.var1=var1;
-        this.var2=var2;
-        this.var3=var3;
+    E[] array;
+
+    public MaxFinder(E[] array){
+        this.array = array;
 
     }
 
     public E  findMax(){
-         return( MaxFinder.findMax(var1,var2,var3));
+        return(findMax(array)) ;
+    }
+    public static <E extends Comparable <E>> E findMax (E[] array) {
+        Arrays.sort(array);
+        E max = array[array.length-1];
+        printMax(array,max);
+        return max;
+    }
 
-    }
-    public static <E extends Comparable <E>>  E findMax (E str1, E str2, E str3) {
-       E max = str1;
-       if(str1.compareTo(max)>0){
-           max = str2;
-       }
-       if(str2.compareTo(max)>0){
-           max=str3;
-       }
-       return max;
-    }
-    public  static <E> void printMax(E num1,E num2,E num3,E max){
-        System.out.printf("The maximum among %s,%s and%s is %s: \n ", num1, num2, num3, max);
+    public  static <E> void printMax(E[] array, E max){
+        System.out.print("The maximum among ");
+        for(int i = 0;i < array.length;i++ ) {
+            System.out.print(array[i]+" ");
+        }
+        System.out.println("is: " +max);
         return;
 
     }
 
     public static void main(String[] args) {
-        Integer int1 = 2 , int2 = 3, int3 = 4;
-        Float float1 = 2f , float2 = 3f , float3 = 4f;
-        String str1="Yash", str2 ="Ambala",str3="Pandemic";
-        Integer x =  new MaxFinder<Integer>(int1,int2,int3).findMax();
-        Float maxFloat = new MaxFinder<Float>(float1,float2,float3).findMax();
-        String maxStr = new MaxFinder<String>(str1,str2,str3).findMax();
-        MaxFinder.printMax(int1,int2,int3,x);
-        MaxFinder.printMax(float1,float2,float3, maxFloat);
-        MaxFinder.printMax(str1,str2,str3,maxStr);
+        Scanner sc = new Scanner(System.in);
+                Integer[] num = {5,10,9} ;
+                Float[] float1 = {6f,79f,88f};
+                String [] str = {"Yash","Zebra","Ambala"};
+
+                Integer x = new MaxFinder<Integer>(num).findMax();
+                Float maxFloat = new MaxFinder<Float>(float1).findMax();
+                String maxStr = new MaxFinder<String>(str).findMax();
+        }
     }
-}
+
--- TU_MaxFinder.java
+++ TU_MaxFinder.java
@@ -6,15 +6,17 @@ import org.junit.jupiter.api.Assertions;
 public class TU_MaxFinder {
     @Test
     void whenIntegerShouldReturnMax() {
-        Integer int1 = 2 , int2 = 3, int3 = 4;
-        Float float1 = 2f , float2 = 3f , float3 = 4f;
-        String str1="Yash", str2 ="Ambala",str3="Pandemic";
-        Integer x = MaxFinder.findMax(int1,int2,int3);
-        Float  y = MaxFinder.findMax(float1,float2,float3);
-        String  z = MaxFinder.findMax(str1,str2,str3);
-        Assertions.assertEquals(4,x,"Validating max method");
-        Assertions.assertEquals(4f,y,"Validating max method");
-        Assertions.assertEquals("Yash",z,"Validating max method");
+        Integer[] num = {5,10,9} ;
+        Float[] float1 = {6f,79f,88f};
+        String [] str = {"Yash","Zebra","Ambala"};
+
+        Integer x = new MaxFinder<Integer>(num).findMax();
+        Float maxFloat = new MaxFinder<Float>(float1).findMax();
+        String maxStr = new MaxFinder<String>(str).findMax();
+        Assertions.assertEquals(10,x,"Validating max method");
+        Assertions.assertEquals(88f,maxFloat,"Validating max method");
+        Assertions.assertEquals("Zebra",maxStr,"Validating max method");
+
 
     }
 }
