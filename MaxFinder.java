89d6d906a67d54edc79cbc58c845b37878b10fe4
hsa-98 <harshsanjayagrawal89@gmail.com>
2021-06-21 13:55:47 +0530

    The code is refactored to take three types of variables in a generic class

--- MaxFinder.java
+++ MaxFinder.java
@@ -1,22 +1,43 @@
 package com.harsh.max;
 
-public class MaxFinder {
-    public static String findMax(String str1, String str2, String str3) {
-       String max = str1;
-       if(str2.compareTo(max)>0){
+public class MaxFinder<E extends Comparable <E>> {
+    E var1,var2,var3;
+    public MaxFinder(E var1,E  var2, E var3){
+        this.var1=var1;
+        this.var2=var2;
+        this.var3=var3;
+
+    }
+
+    public E  findMax(){
+         return( MaxFinder.findMax(var1,var2,var3));
+
+    }
+    public static <E extends Comparable <E>>  E findMax (E str1, E str2, E str3) {
+       E max = str1;
+       if(str1.compareTo(max)>0){
            max = str2;
        }
-       if(str3.compareTo(max)>0){
+       if(str2.compareTo(max)>0){
            max=str3;
        }
-       System.out.printf("Max value of %s %s %s is : %s \n",str1,str2,str3,max);
        return max;
+    }
+    public  static <E> void printMax(E num1,E num2,E num3,E max){
+        System.out.printf("The maximum among %s,%s and%s is %s: \n ", num1, num2, num3, max);
+        return;
+
     }
 
     public static void main(String[] args) {
-        String  str1 ="Harsh";
-        String str2 = "Alabama";
-        String str3 = "Yash";
-        String max = findMax(str1,str2,str3);
+        Integer int1 = 2 , int2 = 3, int3 = 4;
+        Float float1 = 2f , float2 = 3f , float3 = 4f;
+        String str1="Yash", str2 ="Ambala",str3="Pandemic";
+        Integer x =  new MaxFinder<Integer>(int1,int2,int3).findMax();
+        Float maxFloat = new MaxFinder<Float>(float1,float2,float3).findMax();
+        String maxStr = new MaxFinder<String>(str1,str2,str3).findMax();
+        MaxFinder.printMax(int1,int2,int3,x);
+        MaxFinder.printMax(float1,float2,float3, maxFloat);
+        MaxFinder.printMax(str1,str2,str3,maxStr);
     }
 }
--- TU_MaxFinder.java
+++ TU_MaxFinder.java
@@ -6,10 +6,15 @@ import org.junit.jupiter.api.Assertions;
 public class TU_MaxFinder {
     @Test
     void whenIntegerShouldReturnMax() {
-        String num1 = "Harsh";
-        String num2 = "Alabama";
-        String num3= "Yash";
-        String max = MaxFinder.findMax(num1, num2, num3);
-        Assertions.assertEquals("Yash",max,"Validating max method");
+        Integer int1 = 2 , int2 = 3, int3 = 4;
+        Float float1 = 2f , float2 = 3f , float3 = 4f;
+        String str1="Yash", str2 ="Ambala",str3="Pandemic";
+        Integer x = MaxFinder.findMax(int1,int2,int3);
+        Float  y = MaxFinder.findMax(float1,float2,float3);
+        String  z = MaxFinder.findMax(str1,str2,str3);
+        Assertions.assertEquals(4,x,"Validating max method");
+        Assertions.assertEquals(4f,y,"Validating max method");
+        Assertions.assertEquals("Yash",z,"Validating max method");
+
     }
 }
